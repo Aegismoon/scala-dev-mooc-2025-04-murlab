29a0814cc8a42bd3fad5b3dea7824ababd94d36c
Дмитрий Мурат <jamesmuret@gmail.com>
2025-06-29 15:19:32 +0300

    scala 3 style and main

--- scala3Module/src/main/scala/ru/otus/module2/murscala3js.scala
+++ scala3Module/src/main/scala/ru/otus/module2/murscala3js.scala
@@ -3,7 +3,7 @@ package ru.otus.module2
 object murscala3js {
 
     sealed trait JsValue
-  
+
       final case class JsObject(get: Map[String, JsValue]) extends JsValue
 
       final case class JsString(get: String) extends JsValue
@@ -11,49 +11,36 @@ object murscala3js {
       final case class JsNumber(get: Double) extends JsValue
 
       final case object JsNull extends JsValue
-    
 
 
-    trait JsonWriter[T] {
+
+    trait JsonWriter[T]:
       def write(v: T): JsValue
-    }
 
-    object JsonWriter {
 
-      def apply[T](implicit ev: JsonWriter[T]): JsonWriter[T] = ev
-
-      def from[T](f: T => JsValue) = new JsonWriter[T] {
+    object JsonWriter:
+      def apply[T](using ev: JsonWriter[T]): JsonWriter[T] = ev
+      def from[T](f: T => JsValue) = new JsonWriter[T]:
         override def write(v: T): JsValue = f(v)
+
+      // инстансы
+      given  inJsonWriter: JsonWriter[Int] = from[Int](JsNumber(_))
+      given  strJsonWriter: JsonWriter[String] = from[String](JsString)
+
+      given [T: JsonWriter]: JsonWriter[Option[T]] = from {
+      case Some(value) => summon[JsonWriter[T]].write(value)
+      case None => JsNull
       }
-
-      implicit val inJsonWriter: JsonWriter[Int] = from[Int](JsNumber(_))
-
-      implicit val strJsonWriter: JsonWriter[String] = from[String](JsString)
-
-      implicit def optToJsValue[T](implicit ev: JsonWriter[T]): JsonWriter[Option[T]] =
-        from[Option[T]] {
-          case Some(value) => ev.write(value)
-          case None => JsNull
-        }
-    }
-
-    implicit class JsonSyntax[T](v: T) {
-      def toJson(implicit ev: JsonWriter[T]): JsValue = ev.write(v)
-    }
+    extension [T](v: T) def toJson(using ev: JsonWriter[T]): JsValue = ev.write(v)
 
 
-    def toJson[T: JsonWriter](v: T): JsValue = {
-      JsonWriter[T].write(v)
-    }
-
-
-    toJson("fdvvbfbv")
-    toJson(10)
-    toJson(Option(10))
-    toJson(Option("dfbgfgnhg"))
-
-    "bghbbgfrbgbngf".toJson
-    Option(10).toJson
-
+    //def toJson[T: JsonWriter](v: T): JsValue = summon[JsonWriter[T]].write(v)
+    @main def murRunJsonExamples(): Unit =
+    // Примеры использования
+    toJson("murtest") // JsString("mutest")
+    toJson(42) // JsNumber(42.0)
+    toJson(Option(10)) // JsNumber(10.0)
+    "hello".toJson // JsString("hello")
+    Option.empty[Int].toJson // JsNull
 
 }
