6b286798ec824dc7faf0130c678ca4ceeda7148a
Дмитрий Мурат <jamesmuret@gmail.com>
2025-08-24 20:46:50 +0300

    Задание 1

---
+++ scala213Module/src/main/scala/ru/otus/module4/catshttp4shomework/catsefefcthttp4s.scala
@@ -0,0 +1,100 @@
+package catshttp4s
+
+import cats.effect.unsafe.implicits.global
+import cats.effect.{IO, IOApp, Ref}
+import com.comcast.ip4s.{Host, Port}
+import fs2.Stream
+import fs2.io.file.{Files, Path}
+import io.circe.Json
+import org.http4s.circe.jsonEncoder
+import org.http4s.dsl.io._
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Router
+import org.http4s.{Http, HttpRoutes}
+
+object GlobalState {
+
+  val counterRef: Ref[IO, Int] = Ref[IO].of(0).unsafeRunSync()
+
+}
+
+//1
+object Restfull {
+
+
+  val service: HttpRoutes[IO] = HttpRoutes.of {
+    case GET -> Root / "hello" / name => Ok(name)
+  }
+
+
+  def streamChonker: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root / "slow" / chunk / total / time =>
+      val byteStream =
+        Files[IO].readAll(Path("data.txt"))
+          .chunks                     // Stream[IO, Chunk[Byte]]
+          .flatMap(Stream.chunk)      // Stream[IO, Byte] (отдельные байты)
+          .throttle
+          .take(total)
+      Ok(byteStream)    //.map(_.withContentType(`Content-Type`(MediaType.application.`octet-stream`)))
+  }
+
+  def serviceCounter: HttpRoutes[IO] = HttpRoutes.of{
+    case GET -> Root/ "counter" => for {
+      uCntr <- GlobalState.counterRef.updateAndGet(_ + 1)
+      response <- Ok(Json.fromString(s"counter:$uCntr"))
+    } yield response
+  }
+
+  val httpApp: Http[IO, IO] = serviceCounter.orNotFound
+
+  val server1 = for {
+    s <- EmberServerBuilder
+      .default[IO]
+      .withPort(Port.fromInt(8070).get)
+      .withHost(Host.fromString("localhost").get)
+      .withHttpApp(httpApp).build
+  } yield s
+}
+
+object mainServer extends IOApp.Simple {
+    def run(): IO[Unit] = {
+      Restfull.server1.use(_ => IO.never)
+  }
+}
+
+//2
+object RestFull2EndPoints {
+  val serviceOne: HttpRoutes[IO] =
+    HttpRoutes.of {
+      case GET -> Root/ "hello1" / name => Ok(s"web service from $name")
+      case POST -> Root/ "hello2" / name => Ok(s"web service from $name")
+    }
+
+  val serviceTwo: HttpRoutes[IO] = {
+    HttpRoutes.of {
+      case GET -> Root/ "hello2" / name => Ok("web service OK2")
+    }
+  }
+
+  val service : HttpRoutes[IO] = HttpRoutes.of{
+    case GET -> Root / "hello" / name => Ok("web service 0")
+  }
+
+  val router = Router(
+    "/" -> serviceOne,
+    "/api" -> serviceTwo,
+    "/apiroot" -> service
+  )
+
+  val server = EmberServerBuilder
+    .default[IO]
+    .withPort(Port.fromInt(8080).get)
+    .withHost(Host.fromString("localhost").get)
+    .withHttpApp(router.orNotFound).build
+}
+
+object  mainServer2Services extends IOApp.Simple {
+  def run(): IO[Unit] = {
+    RestFull2EndPoints.server.use(_ => IO.never)
+  }
+}
